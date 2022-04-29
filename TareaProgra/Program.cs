2698374cbcad92a7da7405a7996539160588d1ec
Gn3p <Gn3p@DESKTOP-HQFFS0H>
2022-04-29 08:05:55 -0400

    Agregar archivos de proyecto.

---
+++ TareaProgra/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TareaProgra.Comunicacion;
+
+namespace TareaProgra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine(puerto);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                //Servidor conecta a internet
+                Console.WriteLine("Sevidor iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente");
+                    Socket socketCliente = servidor.ObtenerCliente();
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    string mensaje;
+                    string respuesta;
+                    //Esperar Mensaje
+                    int cont = 0;
+                    while (cont == 0)
+                    {
+                        respuesta = cliente.Leer();
+                    if (cliente.LeerMensajeCliente(respuesta))
+                    {
+                        cliente.Desconectar();
+                        Console.WriteLine("El cliente dice: chao");
+                        Console.WriteLine("Anotado en la libreta de cosas que no me importan");
+                            cont = 1;
+                        }
+                    Console.WriteLine("El cliente dice: {0}", respuesta);
+                    Console.WriteLine("Responder:");
+                    mensaje = Console.ReadLine().Trim();
+                    cliente.Escribir(mensaje);
+                    if (cliente.LeerRespuestaServidor(mensaje)){                       {
+                        cliente.Desconectar();
+                        Console.WriteLine("El Servidor dice: chao");
+                        Console.WriteLine("Anotado en la libreta de cosas que no me importan");
+                                cont = 1;
+                            }
+                }
+                }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} esta en uso", puerto);
+            }
+        }
+    }
+}
+    
+
---
+++ TareaPrograCliente/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Configuration;
+using TareaPrograCliente.Comunicacion;
+
+namespace TareaPrograCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+
+            Console.ForegroundColor = ConsoleColor.Green;            
+            Console.WriteLine("Conectando a servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+            if (clienteSocket.Conectar())
+            {
+                //CLIENTE
+                Console.WriteLine("Conectado con el servidor...");
+                string mensaje;
+                string respuesta;
+                int cont = 0;
+                while (cont == 0) {
+                    Console.WriteLine("Enviar Mensaje:");
+                    mensaje = Console.ReadLine().Trim();
+                    clienteSocket.Escribir(mensaje);
+                    if (mensaje == "chao")
+                    {
+                        clienteSocket.Desconectar();
+                        Console.WriteLine("Al cabo que ni queria seguir hablando contigo");
+                        cont = 1;
+                    }
+                    respuesta = clienteSocket.Leer();
+                    Console.WriteLine("R: {0}", respuesta);
+                    if (respuesta == "chao")
+                    {
+                        clienteSocket.Desconectar();
+                        Console.WriteLine("Al cabo que ni queria seguir hablando contigo");
+
+                        cont = 1;
+
+                    }
+                    
+
+                }
+                Console.WriteLine("saliendo del while");
+
+            }
+            else
+            {
+                Console.WriteLine("Error de comunicacion...");
+            }
+            Console.ReadKey();
+
+        }
+        
+
+    }
+  
+
+    }
+
+ 
